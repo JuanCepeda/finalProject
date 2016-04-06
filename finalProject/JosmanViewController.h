f2665544fb24ed78b1df25acf6144815061b1183
josman185hotm <josman185@gmail.com>
2016-04-05 23:12:40 -0400

    This is my first commit

---
+++ finalProject/JosmanViewController.h
@@ -0,0 +1,13 @@
+//
+//  JosmanViewController.h
+//  finalProject
+//
+//  Created by MCS on 4/5/16.
+//  Copyright © 2016 MCS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface JosmanViewController : ViewController
+
+@end
